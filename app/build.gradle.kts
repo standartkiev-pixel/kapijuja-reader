dacb6fdbf387951b16b5738f6c1c62c0ca8ca701
standartkiev-pixel <standart.kiev@gmail.com>
2026-09-07 17:59:18 +0200

    Bump Kapijuja Reader to 0.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kapijuja.reader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
     }
 
     signingConfigs {
